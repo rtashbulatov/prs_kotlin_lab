e272a14e380c3526c1a44ba6c9b953858bd87de9
rtashbulatov <rtashbulatov@sdvor.com>
2023-10-11 23:23:43 +0500

    добавил ещё один вариант перемножения матриц

--- src/main/kotlin/lab2/MatrixMain.kt
+++ src/main/kotlin/lab2/MatrixMain.kt
@@ -1,38 +1,42 @@
 package lab2
 
+import java.util.Random
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import java.util.stream.IntStream
 import kotlin.system.measureTimeMillis
 
 fun main() {
+    val rnd = Random(123)
     val n = 1000
     val m = 1000
     val k = 1000
-    val a = createMatrix(n, m)
-    val b = createMatrix(m, k)
-    val time1 = measureTimeMillis { mul(a, b) }
-    val time2 = measureTimeMillis { parallelMul(a, b, 8) }
+    val a = createMatrix(n, m, rnd)
+    val b = createMatrix(m, k, rnd)
+
+    var res1: Array<IntArray>
+    var res2: Array<IntArray>
+    var res3: Array<IntArray>
+    val time1 = measureTimeMillis { res1 = mul(a, b) }
+    val time2 = measureTimeMillis { res2 = parallelMul(a, b, 4) }
+    val time3 = measureTimeMillis { res3 = parallelMul2(a, b, 4) }
+    assert(equals(res1, res2))
+    assert(equals(res2, res3))
     println(time1)
     println(time2)
-//    val c = mul(a, b)
-//    val cParallel1 = parallelMul(a, b)
-//    print(a)
-//    print(b)
-//    print(c)
-//    print(cParallel1)
+    println(time3)
 }
 
-fun createMatrix(n: Int, m: Int, zero: Boolean = false): Array<IntArray> {
+fun createMatrix(n: Int, m: Int, random: Random? = null): Array<IntArray> {
     return Array(n) {
         IntArray(m) {
-            if (!zero) (0..100).random() else 0
+            random?.nextInt() ?: 0
         }
     }
 }
 
 fun mul(a: Array<IntArray>, b: Array<IntArray>): Array<IntArray> {
-    val res = createMatrix(a.size, b[0].size, true)
+    val res = createMatrix(a.size, b[0].size)
 
     for (i in res.indices) {
         for (j in res[0].indices) {
@@ -60,17 +64,38 @@ fun parallelMul(a: Array<IntArray>, b: Array<IntArray>, threadNum: Int = 1): Arr
     return res
 }
 
+fun parallelMul2(a: Array<IntArray>, b: Array<IntArray>, threadNum: Int = 1): Array<IntArray> {
+    val res = createMatrix(a.size, b[0].size)
+    val executor = Executors.newFixedThreadPool(threadNum)
+
+    for (i in 0..<threadNum) {
+        executor.execute {
+            blockMul(a, b, res, threadNum, i)
+        }
+    }
+
+    executor.shutdown()
+    executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)
+    return res
+}
+
+fun blockMul(a: Array<IntArray>, b: Array<IntArray>, res: Array<IntArray>, threadNum: Int, threadIdx: Int) {
+    val blockSize = res.size * res[0].size / threadNum
+    val handledSize = threadIdx * blockSize
+    val realBlockSize = (res.size * res[0].size - handledSize).coerceAtMost(blockSize)
+    for (k in handledSize..<handledSize + realBlockSize) {
+        val i = k % res[0].size
+        val j = k / res[0].size
+        res[i][j] = mul(a, b, i, j)
+    }
+}
+
 fun mul(a: Array<IntArray>, b: Array<IntArray>, row: Int, col: Int): Int {
     return IntStream.range(0, b.size)
         .map { i -> a[row][i] * b[i][col] }
         .sum()
 }
 
-fun print(matrix: Array<IntArray>) {
-    matrix.forEach {row ->
-        row.forEach {
-            print("$it ")
-        }
-        println()
-    }
+fun equals(a: Array<IntArray>, b: Array<IntArray>): Boolean {
+    return a.contentDeepEquals(b)
 }
